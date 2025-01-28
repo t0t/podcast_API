777a2d954a87ef5e62fc85e237485463955f603f
sergio fores <sergiofores@gmail.com>
2025-01-28 17:16:03 +0100

    anadidos audios

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   base: '/podcast_API/',
   build: {
     outDir: 'dist'
+  },
+  server: {
+    cors: true
   }
-}
+})
